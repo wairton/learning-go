4f1c8d6a40e6e8b001206b8d5c4212c306f57fbf
Wairton Rebouças <wairtonjr@gmail.com>
2024-04-02 23:01:15 -0300

    add InfoHeader

--- bmp/main.go
+++ bmp/main.go
@@ -23,10 +23,42 @@ func NewHeader(data []byte, offset int) Header {
 	}
 }
 
+type InfoHeader struct {
+	size            uint32
+	width           uint32
+	height          uint32
+	planes          uint16
+	bpp             uint16
+	compression     uint32
+	imageSize       uint32
+	xpixels         uint32
+	ypixels         uint32
+	colors          uint32
+	importantColors uint32
+}
+
+func NewInfoHeader(data []byte, offset int) InfoHeader {
+	return InfoHeader{
+		size:            binary.LittleEndian.Uint32(data[offset+0:]),
+		width:           binary.LittleEndian.Uint32(data[offset+4:]),
+		height:          binary.LittleEndian.Uint32(data[offset+8:]),
+		planes:          binary.LittleEndian.Uint16(data[offset+12:]),
+		bpp:             binary.LittleEndian.Uint16(data[offset+14:]),
+		compression:     binary.LittleEndian.Uint32(data[offset+16:]),
+		imageSize:       binary.LittleEndian.Uint32(data[offset+20:]),
+		xpixels:         binary.LittleEndian.Uint32(data[offset+24:]),
+		ypixels:         binary.LittleEndian.Uint32(data[offset+28:]),
+		colors:          binary.LittleEndian.Uint32(data[offset+32:]),
+		importantColors: binary.LittleEndian.Uint32(data[offset+36:]),
+	}
+}
+
 func main() {
 	dat, err := ioutil.ReadFile("file/480-360-sample.bmp")
 	header := NewHeader(dat, 0)
+	infoHeader := NewInfoHeader(dat, 14)
 	fmt.Println(header)
+	fmt.Println(infoHeader)
 	if err != nil {
 		log.Fatal("nope")
 	}
